eec20fa6e89d59d24710f1bd48f627f7adc6cccf
AbdielDiazPalomino <abdoljdp@gmail.com>
2025-06-27 19:02:09 -0500

    settings

--- proyecto_final_backend/settings.py
+++ proyecto_final_backend/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'proyecto_final_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bbdd-II-proyecto-final',  # Se queda con museos nuv
+        'NAME': 'bbdd-II-proyecto-final', 
         'USER': 'postgres',
         'PASSWORD': '1234',
         'HOST': 'localhost',
